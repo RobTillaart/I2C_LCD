d8ca4097856333cb129fe5b97ca6695206672606
Rob Tillaart <rob.tillaart@gmail.com>
2024-08-26 13:59:40 +0200

    Fix #11 and some more

--- I2C_LCD_special_chars.h
+++ I2C_LCD_special_chars.h
@@ -15,8 +15,9 @@ const char LCD_ALPHA    = 0xE0;
 const char LCD_BETA     = 0xE2;
 const char LCD_EPSILON  = 0xE3;
 const char LCD_MU       = 0xE4;
-const char LCD_RHO      = 0xE5;
-const char LCD_SQROOT   = 0xE7;
+// const char LCD_???    = 0xE5;
+const char LCD_RHO      = 0xE6;
+const char LCD_SQROOT   = 0xE8;
 
 const char LCD_THETA    = 0xF2;
 const char LCD_INFINITY = 0xF3;
